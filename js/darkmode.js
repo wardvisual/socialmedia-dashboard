d9a3c8844df2488ac613b3c845ffbd47c10a5355
constWardtz <edwardtutorials22@gmail.com>
2021-03-01 08:44:54 +0700

    Update Naming Convention

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,6 @@
 const switchBtn = document.querySelector(`#hero__switch`);
 const btnSlider = document.querySelector(`#hero__switch--slider`);
-const main = document.querySelector(`#main`);
+const hero = document.querySelector(`#hero`);
 const toggleName = document.querySelector(`#hero__switch--name`);
 
 switchBtn.addEventListener(`click`, () => {
@@ -34,18 +34,18 @@ switchBtn.addEventListener(`click`, () => {
 });
 
 const darkmode = () => {
-  main.classList.contains("hero--theme-dark") &&
-    main.classList.remove("hero--theme-dark");
-  main.classList.add("hero--theme-light");
+  hero.classList.contains("hero--theme--dark") &&
+    hero.classList.remove("hero--theme--dark");
+  hero.classList.add("hero--theme--light");
   document.body.style = `
     background-color: var(--white);
   `;
 };
 
 const lightmode = () => {
-  main.classList.contains("hero--theme-light") &&
-    main.classList.remove("hero--theme-light");
-  main.classList.add("hero--theme-dark");
+  hero.classList.contains("hero--theme--light") &&
+    hero.classList.remove("hero--theme--light");
+  hero.classList.add("hero--theme--dark");
   document.body.style = `
     background-color: var(--very-dark-blue2);
     `;
